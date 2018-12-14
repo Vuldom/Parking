0c96299892171293e7893e3a07b908401541248a
maryia.paliashchuk@gmail.com <maryia.paliashchuk@gmail.com>
2018-12-14 14:18:10 +0300

    first project

--- Educo.Parking.Data/obj/Release/netcoreapp2.1/Educo.Parking.Data.AssemblyInfo.cs
+++ Educo.Parking.Data/obj/Release/netcoreapp2.1/Educo.Parking.Data.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Educo.Parking.Shell.Web")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Educo.Parking.Data")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Educo.Parking.Shell.Web")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Educo.Parking.Shell.Web")]
+[assembly: System.Reflection.AssemblyProductAttribute("Educo.Parking.Data")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Educo.Parking.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
